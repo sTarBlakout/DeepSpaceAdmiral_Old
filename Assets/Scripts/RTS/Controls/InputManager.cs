da7f417708554661e85036c25966ac6d5e7b1b32
sTarBlakout <46494422+sTarBlakout@users.noreply.github.com>
2020-08-22 00:10:46 +0300

    Started with structurizing prototype movement.

---
+++ Assets/Scripts/RTS/Controls/InputManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Lean.Touch;
+using RTS.Ships;
+
+namespace RTS.Controls
+{
+    public class InputManager : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            LeanTouch.OnFingerTap += HandleFingerTap;
+        }
+
+        private void OnDisable()
+        {
+            LeanTouch.OnFingerTap -= HandleFingerTap;
+        }
+
+        private void HandleFingerTap(LeanFinger finger)
+        {
+            if (Physics.Raycast(finger.GetRay(), out var hitInfo, Mathf.Infinity))
+            {
+                var battleship = hitInfo.collider.GetComponent<BattleshipBase>();
+                if (battleship != null)
+                {
+                    Debug.Log("Tapped on Battleship!");
+                    return;
+                }
+                
+                Debug.Log("Tapped on Infinite Space!");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RTS/Ships/BattleShipBase.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace RTS.Ships
+{
+    public class BattleshipBase : MonoBehaviour
+    {
+        [SerializeField] private float movementSpeed = 1f;
+        [SerializeField] private float rotationSpeed = 1f;
+
+        private Rigidbody _rigidbody;
+        
+        private bool _isSelected;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+    }
+}
